20bce982cc3054e8d08c225944d357128d689ce2
uchiyama33 <33150646+uchiyama33@users.noreply.github.com>
2022-08-04 09:27:35 +0000

    del envs

--- envs/org3dresnet_installer/org3dresnet_setup.py
+++
@@ -1,9 +0,0 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
-
-setup(
-    name="org3dresnet",
-    packages=find_packages(),
-)
